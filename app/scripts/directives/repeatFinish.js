be4f105112d6668857b8ef74e6ad6820c0f65235
Carter Tsai <hamming1@gmail.com>
2014-03-03 23:50:22 +0800

    Add repeatFinish.js for handle repeat finish event

---
+++ app/scripts/directives/repeatFinish.js
@@ -0,0 +1,15 @@
+'use strict';
+// return the function's  return value
+angular.module('styleApp')
+.directive('onFinishRenderFilters', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (scope.$last === true) {
+                    $timeout(function () {
+                        scope.$emit('ngRepeatFinished');
+                    });
+                }
+            }
+        }
+    });
